eefae4b2206465d5a2a2506181f8edbd0460d9a3
bellobambo <bellobambo21@gmail.com>
2023-07-14 20:43:36 +0100

    final lap

---
+++ webscrapping4.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+gpu = input("what Product do you want? ")
+
+
+# Get number of pages
+url = f'https://www.newegg.ca/p/pl?d={gpu}&N=4808'
+page = requests.get(url).text
+doc = BeautifulSoup(page , "html.parser")
+page_text = doc.find(class_="list-tool-pagination-text" ).strong
+pages = int(str(page_text).split('/')[-2].split(">")[-1][:-1])
+print(pages)
+
+
