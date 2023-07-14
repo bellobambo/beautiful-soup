b4d5c07f3958684287496599c264e60c98cda065
bellobambo <bellobambo21@gmail.com>
2023-07-14 20:46:34 +0100

    second commit

--- webscrapping4.py
+++ webscrapping4.py
@@ -13,4 +13,9 @@ page_text = doc.find(class_="list-tool-pagination-text" ).strong
 pages = int(str(page_text).split('/')[-2].split(">")[-1][:-1])
 print(pages)
 
+# looping through the pages to get the number
+for page in range(pages):
+    url = f'https://www.newegg.ca/p/pl?d={gpu}&N=4808'
+    page = requests.get(url).text
+    doc = BeautifulSoup(page , "html.parser")
 
